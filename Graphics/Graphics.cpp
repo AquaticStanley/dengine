9c69a64f8f8b709996f7867f28dcadf8cbc54763
Tristan <tfount96@gmail.com>
2020-07-12 12:38:45 -0500

    Initial commit to tile-based physics-ey turn based game thing

---
+++ Graphics/Graphics.cpp
@@ -0,0 +1,32 @@
+#include "Graphics.h"
+
+void Graphics::draw(sf::Vector2f position, sf::Vector2f hitbox, sf::Color color) {
+    sf::RectangleShape rectangle(hitbox);
+    rectangle.setOrigin(0, rectangle.getSize().y);
+    rectangle.setFillColor(color);
+
+    //Translate position
+    position = translatePosition(position);
+
+    rectangle.setPosition(position);
+
+    window_->draw(rectangle);
+    return;
+}
+
+void Graphics::draw(sf::Vector2f position, sf::Sprite sprite) {
+    sf::Vector2f roundedPosition = position;//sf::Vector2f(position.x - 0.5, position.y - 0.5);
+
+    //Translate position
+    roundedPosition = translatePosition(roundedPosition);
+
+    //Set Position
+    sprite.setPosition(roundedPosition);
+
+    window_->draw(sprite);
+    return;
+}
+
+sf::Vector2f Graphics::translatePosition(sf::Vector2f position) {
+    return sf::Vector2f(position.x, window_->getSize().y - position.y);
+}
---
+++ Graphics/Graphics.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+class Graphics {
+public:
+    Graphics(sf::RenderWindow* window) : window_(window) {}
+
+    void draw(sf::Vector2f position, sf::Vector2f hitbox, sf::Color color);
+    void draw(sf::Vector2f position, sf::Sprite);
+
+    sf::Vector2f translatePosition(sf::Vector2f position);
+
+private:
+    sf::RenderWindow* window_;
+};
